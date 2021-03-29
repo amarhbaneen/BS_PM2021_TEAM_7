041f80f8a518916b8a6361e36e6391fd512e54da
tmem11 <tmem.nsasra@gmail.com>
2021-03-29 17:43:05 +0300

    register models to the project

--- schoolSystemManagment/admin.py
+++ schoolSystemManagment/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Admin)
+admin.site.register(HomeWork)
+admin.site.register(StudentSolution)
+admin.site.register(Studies)
+admin.site.register(AdminMessage)
+admin.site.register(TeacherMessage)
+admin.site.register(Grade)
+admin.site.register(StudiesStudent)
