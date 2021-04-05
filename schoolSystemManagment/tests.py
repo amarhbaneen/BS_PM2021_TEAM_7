c4836aebad1360341da7825485b362959deb981c
amara <amaralzana@gmail.com>
2021-04-05 17:06:39 +0300

    add unittest for Addhomework

--- schoolSystemManagment/tests.py
+++ schoolSystemManagment/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase, Client
+
+
+class TestAddHomeWork(TestCase):
+    def AddHomeWork_test(self):
+        client = Client()
+        response = client.get('/homeworkform/')
+        self.assertTemplateUsed(response,"homework_form.html")
+        self.assertEquals(response.status_code,200)
