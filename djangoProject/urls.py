3ecdaa938afa531deab5a8d03ba6d28ab2931ce9
tmem11 <tmem.nsasra@gmail.com>
2021-04-05 17:09:03 +0300

    adding tests to the AddAmessages view

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -23,7 +23,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('addMessages/', views.addMessage, name='aa'),
+    path('addMessages/', views.addMessage, name='createMess'),
     path('index/', views.index, name='aa'),
 
 ]
--- schoolSystemManagment/tests.py
+++ schoolSystemManagment/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+from django.urls import reverse
 
 # Create your tests here.
+from djangoProject.views import addMessage
+from .models import AdminMessage
+from .forms import AdminMessageForm
+class AdminMessageFormTests(TestCase):
+    def test_Add_Message_GET(self):
+        c=Client()
+        response=c.get(reverse('createMess'))
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'AddAmessages.html')
+
+    def test_Add_Message_POST(self):
+        c = Client()
+        data = {'messageContent': 'this is a test for the post method'}
+
+        responce = c.post(reverse('createMess'))
+        self.assertEquals(responce.status_code, 302)
+        self.assertTemplateNotUsed(responce,'index.html')
+
+
+
+
+
+
+
