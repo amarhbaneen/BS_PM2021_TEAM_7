0f9d74e7d7f4505434f892546f0d84829d53e6c4
Somaa <alturso@ac.sce.ac.il>
2021-04-10 20:18:52 +0300

    complate the first requirment ,update

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,15 +1,29 @@
+from django.contrib import messages
+from django.contrib.auth.models import User
 from django.shortcuts import render,redirect
-from .forms import addUserForm
+from djangoProject.forms import addUserForm
+from schoolSystemManagment.models import models,Student,Teacher
+
 
 def Teacherbase(request):
     return render(request,"teacher_base.html")
 
-
-def addUser(response):
-    if response.method=='POST':
-        form=addUserForm(response.POST)
+def addUser(request):
+    if request.method=='POST':
+        form=addUserForm(request.POST)
         if form.is_valid():
-            form.save()
+            curr_user=form.save()
+            if form.clean_user_type() in('Student','student'):
+                teacher = Teacher.objects.get(userId__username=form.clean_chose_teacher()) ###
+                Student.objects.create(userId=curr_user,teacherId_id=teacher.userId)
+                print(teacher.userId)
+                ###
+
+            elif form.clean_user_type() in('Teacher','teacher'):
+                Teacher.objects.create(userId=curr_user)
     else:
         form=addUserForm()
-    return render(response,"addUser.html",{"form":form})
+    return render(request,"addUser.html",{"form":form})
+
+def checkValue(value):
+    return value!=None and value==True
