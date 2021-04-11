cce039567e3a3e993efb80db931d4f0807a0f2c3
amarhbaneen <amaralzana@gmail.com>
2021-04-11 10:02:52 +0300

    added test for deleting homework and changed the inserting homwork test

--- schoolSystemManagment/tests.py
+++ schoolSystemManagment/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase,Client
+from django.test import TestCase, Client
 from django.urls import reverse
 
 # Create your tests here.
@@ -6,21 +6,23 @@ from djangoProject.views import addMessage
 from .models import AdminMessage
 from .forms import AdminMessageForm
 
+
+
 class TestAddHomeWork(TestCase):
-    def AddHomeWork_test(self):
+    def test_AddHomeWork(self):
         client = Client()
-        response = client.get('/homeworkform/')
-        self.assertTemplateUsed(response,"homework_form.html")
-        self.assertEquals(response.status_code,200)
+        response = client.get('homework_form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, "homework_form.html")
 
 
 
 class AdminMessageFormTests(TestCase):
     def test_Add_Message_GET(self):
-        c=Client()
-        response=c.get(reverse('createMess'))
-        self.assertEquals(response.status_code,200)
-        self.assertTemplateUsed(response,'AddAmessages.html')
+        c = Client()
+        response = c.get(reverse('createMess'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'AddAmessages.html')
 
     def test_Add_Message_POST(self):
         c = Client()
@@ -28,11 +30,12 @@ class AdminMessageFormTests(TestCase):
 
         responce = c.post(reverse('createMess'))
         self.assertEquals(responce.status_code, 302)
-        self.assertTemplateNotUsed(responce,'index.html')
+        self.assertTemplateNotUsed(responce, 'index.html')
 
 
-
-
-
-
-
+class DeletingHomework(TestCase):
+    def test_delete_homework_GET(self):
+        c = Client()
+        response = c.get(reverse('homework_delete', 0))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'teacher_base.html')
