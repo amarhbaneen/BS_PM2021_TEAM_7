680ebd6267d4a5519416a03b135f2764adf8eddb
bahjat s <bahjans@ac.sce.ac.il>
2021-04-22 17:30:53 +0300

    BSPM2021T7-17 בתור מורה ארצה שתהיה לי את האפשרות להכנס למערכת על מנת להשתמש במערכת
    BSPM2021T7-54 יצירת דף כניסה
    BSPM2021T7-56 יצירת כפתור כניסה
    BSPM2021T7-58 חיבור עם db
    
    הוספתי if לפונקציה login וטיפלתי במצב שנכנס בו מורה
    יצרתי דף מורה התחלתי
    בדקתי אם מורה קיים בבסיס ניתונים
    
    BSPM2021T7-30
    אני כתלמיד רוצה שתהיה לי את האפשרות להיכנס למערכת כדי לצפות בפעולות המערכת
    
    BSPM2021T7-72 יצירת דף כניסה
    BSPM2021T7-73 יצירת כפתור כניסה
    BSPM2021T7-74 חיבור עם db
    
    הוספתי if לפונקציה login וטיפלתי במצב שנכנס בו תלמיד
    יצרתי דף תלמיד התחלתי
    בדקתי אם התלמיד קיים בבסיס ניתונים

--- App/urls.py
+++ App/urls.py
@@ -1,12 +1,9 @@
-
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
 
-    path('homepage',views.homepage,name='homepage')
-
-
-
+    path('homepage', views.homepage, name='homepage'),
+    path('login/', views.login, name='login'),
 
 ]
--- App/views.py
+++ App/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def homepage(request):
-    return  render(request,'dashboard.html')
+    return render(request, 'dashboard.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.groups.filter(name='admins').exists():
+            auth.login(request, user)
+            return render(request, 'AdminPage.html')
+        elif user is not None and user.groups.filter(name='students').exists():
+            auth.login(request, user)
+            return render(request, 'StudentPage.html')
+        elif user is not None and user.groups.filter(name='teachers').exists():
+            auth.login(request, user)
+            return render(request, 'TeacherPage.html')
+        else:
+            messages.info(request, 'error')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
