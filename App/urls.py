aef3bc5b9264a97686d4e9abf90a61dd763e1ec2
amarhbaneen <amaralzana@gmail.com>
2021-04-23 03:08:28 +0300

    BSPM2021T7-1 אני כמורה יכול להוסיף משימה חדש לסטודנטים שלי על מנת לתן לסטודנטים לפתור אותה BSPM2021T7-15 ליצר כפתור שמאפשר למורה להוסיף משימה חדשה FRONT END BSPM2021T7-16 FRONT END יצירת דף שמכיל תבנית חדשה למשימה BSPM2021T7-115 BACK END פונוקציה שמעבירה את המשתמש לדף אחרי לחיצת כפתור ההוספה BSPM2021T7-116 BACK END שמירה בבסיסי נתונים המשימה החדשה שהתקבלה ###################################################################### BSPM2021T7-3 אני כמורה יכול לערוך משימה קיימת על מנת לתקן BSPM2021T7-24 יצירת כפתור עריכה FRONT END BSPM2021T7-25 יצירת דף עריכה FRONT END BSPM2021T7-120 BACK END פונוקציה שמעבירה את המשתמש לדף אחרי לחיצת כפתור עריכה BSPM2021T7-121 BACK END שמירה בבסיסי נתונים הנתונים החדשים אחרי עריכה

--- App/urls.py
+++ App/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
 
     path('homepage', views.homepage, name='homepage'),
     path('login/', views.login, name='login'),
-
+    path('teacher', views.teacher_dashboard, name='teacher'),
+    path('delete_homework/<int:id>/', views.homework_delete, name='homework_delete'),
+    path('homeworkform/', views.homework_form, name='homework_form'),
+    path('homeowrk<int:id>/', views.homework_form, name='homework_update'),
 ]
