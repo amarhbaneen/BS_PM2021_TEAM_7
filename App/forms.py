09f5a8ca84fbf9e945f19277a381ee19fd546bf7
Somaa <alturso@ac.sce.ac.il>
2021-05-31 13:30:46 +0300

    split add user (user story),to two separate action ,adding for Student,Teacher

--- App/forms.py
+++ App/forms.py
@@ -56,46 +56,11 @@ class BugReportForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(BugReportForm, self).__init__(*args, **kwargs)
 
-'''queryset =Teacher.objects.all()
-teacher_id=[p.userId for p in queryset]'''
 
 class addUserForm(UserCreationForm):
-    email=forms.EmailField()
-    user_type=forms.CharField(max_length=50,required=True)
-    chose_teacher = forms.CharField(max_length=50,required=False)
-    class Meta:
-        model=User
-        fields=['username','email','user_type','chose_teacher','password1','password2']
-
-    def clean_email(self):
-        email=self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('This mail is all ready in system')
-        elif not email.endswith('com') and not email.endswith('il'):
-            raise forms.ValidationError('This is not a valid email')
-        return email
-
-    def clean_user_type(self):
-        user_type=self.cleaned_data.get('user_type')
-        if user_type not in ('teacher','student','Student','Teacher'):
-            raise forms.ValidationError('you need to chose one of this [Student,Teacher]')
-        else:
-            return user_type
+   def clean_username(self,*args,**kwargs):
+       user_name=self.cleaned_data.get('username')
+       if User.objects.filter(username=user_name).exists():
+           raise forms.ValidationError("this name is already exist")
 
 
-    def clean_chose_teacher(self):
-        user_type=self.clean_user_type()
-        chose_teacher = self.cleaned_data.get('chose_teacher')
-        user = User.objects.get(username=chose_teacher)
-        if user_type in ('teacher','Teacher'):
-            if chose_teacher !='':
-                raise forms.ValidationError('keep this box empty for teacher')
-            else:
-                return chose_teacher
-        elif user_type in ('student','Student'):
-            if chose_teacher == '':
-                raise forms.ValidationError('you need to pick a teacher')
-            elif not  Teacher.objects.filter(user=user).exists():
-                raise forms.ValidationError('Teacher name is  not existing ')
-            else:
-                return chose_teacher
